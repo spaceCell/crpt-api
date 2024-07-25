4e3406b4a1c9c89b22bacc49561c109cce8ea0a5
Kirill Kozlov <space_cell@proton.me>
2024-07-25 16:09:22 +0500

    Init app

---
+++ src/test/java/org/kirill/space/cell/crpt_api/CrptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kirill.space.cell.crpt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrptApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
